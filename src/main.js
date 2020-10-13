c16d7b86f1bac2f6439d013d6dc2e4293ac117a1
Maya Soloveva <maya.soloveva2015@yandex.ru>
2020-10-13 23:51:15 +0300

    markup to main.js

---
+++ src/main.js
@@ -0,0 +1,477 @@
+console.log('main.js');
+
+const LOCALE = 'en-GB';
+const DATE_OPTS = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+const DAY_OPTS = { month: 'long', day: 'numeric' };
+const TIME_OPTS = { hour: 'numeric', minute: 'numeric' };
+const REPEAT_INPUTS = [
+  'mo',
+  'tu',
+  'we',
+  'th',
+  'fr',
+  'sa',
+  'su'
+]
+
+
+const CARDS = [
+  {
+    classes: ['black'],
+    text: 'Example task with default color.',
+    date: new Date(2020, 8, 23, 12, 45)
+  },
+  {
+    classes: ['blue'],
+    text: 'Example task with custom color.',
+    date: new Date(2020, 9, 2, 1, 45)
+  },
+  {
+    classes: ['yellow'],
+    text: 'Example task with custom color and without date.'
+  },
+  {
+    classes: ['green'],
+    text: 'Example task with custom color.',
+    date: new Date(2020, 9, 2, 1, 45)
+  },
+  {
+    classes: ['black'],
+    text: 'Example task without date.'
+  },
+  {
+    classes: ['pink', 'repeat'],
+    text: 'It is example of repeating task. It marks by wave.',
+    date: new Date(2020, 9, 2, 1, 45)
+
+  },
+  {
+    classes: ['red', 'deadline'],
+    text: 'This is task with missing deadline.',
+  },
+  {
+    classes: ['black', 'deadline'],
+    text: 'This is task with missing deadline. Deadline always marked by red line.',
+    date: new Date(2020, 9, 2, 1, 45)
+  }
+];
+
+const getControlTmplt = () => {
+  return (
+    `<section class="control__btn-wrap">
+      <input
+        type="radio"
+        name="control"
+        id="control__new-task"
+        class="control__input visually-hidden"
+      />
+      <label for="control__new-task" class="control__label control__label--new-task"
+        >+ ADD NEW TASK</label
+      >
+      <input
+        type="radio"
+        name="control"
+        id="control__task"
+        class="control__input visually-hidden"
+        checked
+      />
+      <label for="control__task" class="control__label">TASKS</label>
+      <input
+        type="radio"
+        name="control"
+        id="control__statistic"
+        class="control__input visually-hidden"
+      />
+      <label for="control__statistic" class="control__label"
+        >STATISTICS</label
+      >
+    </section>`
+  );
+}
+
+const getFilterTmplt = () => {
+  return (
+      `      <section class="main__filter filter container">
+        <input
+          type="radio"
+          id="filter__all"
+          class="filter__input visually-hidden"
+          name="filter"
+          checked
+        />
+        <label for="filter__all" class="filter__label">
+          All <span class="filter__all-count">13</span></label
+        >
+        <input
+          type="radio"
+          id="filter__overdue"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+        />
+        <label for="filter__overdue" class="filter__label"
+          >Overdue <span class="filter__overdue-count">0</span></label
+        >
+        <input
+          type="radio"
+          id="filter__today"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+        />
+        <label for="filter__today" class="filter__label"
+          >Today <span class="filter__today-count">0</span></label
+        >
+        <input
+          type="radio"
+          id="filter__favorites"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__favorites" class="filter__label"
+          >Favorites <span class="filter__favorites-count">1</span></label
+        >
+        <input
+          type="radio"
+          id="filter__repeating"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__repeating" class="filter__label"
+          >Repeating <span class="filter__repeating-count">1</span></label
+        >
+        <input
+          type="radio"
+          id="filter__archive"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__archive" class="filter__label"
+          >Archive <span class="filter__archive-count">115</span></label
+        >
+      </section>`
+  );
+}
+
+const getBoardFilterTmplt = () => {
+  return (
+`<div class="board__filter-list">
+  <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a
+  ><a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a
+  ><a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+</div>`
+  );
+}
+
+const getClassList = (classArr, prefix) => {
+    const classes = classArr
+      .reduce((classes,classItem) => {
+        return `${classes} ${prefix}--${classItem}`;
+      }, prefix);
+
+    return classes;
+}
+
+const getTaskCardTmplt = (classList = {classes: ['black'], text: 'default text'}) => {
+  const classes = getClassList(classList.classes,'card');
+
+  let date = '';
+  if (typeof classList.date !== 'undefined') {
+    date = (
+      `<div class="card__dates">
+        <div class="card__date-deadline">
+          <p class="card__input-deadline-wrap">
+            <span class="card__date">${classList.date.toLocaleString(LOCALE, DAY_OPTS)}</span>
+            <span class="card__time">${classList.date.toLocaleString(LOCALE, TIME_OPTS)}</span>
+          </p>
+        </div>
+      </div>`
+    );
+  }
+
+  return (
+    `<article class="card ${classes}">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            <button type="button" class="card__btn card__btn--edit">
+              edit
+            </button>
+            <button type="button" class="card__btn card__btn--archive">
+              archive
+            </button>
+            <button
+              type="button"
+              class="card__btn card__btn--favorites card__btn--disabled"
+            >
+              favorites
+            </button>
+          </div>
+
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <p class="card__text">${classList.text}</p>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              ${date}
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>`
+  );
+
+}
+
+const getDateInputTmplt = (datePars = {placeholder: ''}) => {
+
+  let dateStatus = 'no';
+  let value = '';
+  let disabled = 'disabled';
+
+  if (typeof datePars.date !== 'undefined') {
+    dateStatus= 'yes';
+    value = datePars.date.toLocaleString(LOCALE, DATE_OPTS);
+    disabled ='';
+  }
+
+  return (
+    `<button class="card__date-deadline-toggle" type="button">
+      date: <span class="card__date-status">${dateStatus}</span>
+    </button>
+
+    <fieldset class="card__date-deadline" ${disabled}>
+      <label class="card__input-deadline-wrap">
+        <input
+          class="card__date"
+          type="text"
+          placeholder="${datePars.placeholder}"
+          name="date"
+          value="${value}"
+        />
+      </label>
+    </fieldset>`
+  );
+
+};
+
+const getRepeatItemTmplt = (label, isChecked = false, number = 1) => {
+  console.log('getRepeatItemTmplt');
+  console.log(label);
+  if (!label) {
+    return;
+  }
+
+  let checked = '';
+  let id = `repeat-${label}-${number}`
+
+  if (isChecked) {
+    checked = 'checked';
+  }
+
+  return (
+    `<input
+      class="visually-hidden card__repeat-day-input"
+      type="checkbox"
+      id="${id}"
+      name="repeat"
+      value="${label}"
+      ${checked}
+    />
+    <label class="card__repeat-day" for="${id}"
+      >${label}</label
+    >`
+  );
+}
+
+const getRepeatInputTmplt = (repeatTmplt = '0000000') => {
+
+  const sum = repeatTmplt
+    .split('')
+    .reduce((arr,el) => {
+      return parseInt(arr) + parseInt(el);
+    },0)
+
+  let isRepeat = 'no';
+  let disabled = 'disabled';
+
+  if (sum > 0) {
+    isRepeat = 'yes';
+    disabled = '';
+  }
+
+  const repeatInputs = REPEAT_INPUTS
+    .reduce((result,label, i) => {
+      return result + getRepeatItemTmplt(label, parseInt(repeatTmplt[i]), 1);
+    },``);
+
+  return (
+    `<button class="card__repeat-toggle" type="button">
+      repeat:<span class="card__repeat-status">${isRepeat}</span>
+    </button>
+    <fieldset class="card__repeat-days" ${disabled}>
+      <div class="card__repeat-days-inner">
+        ${repeatInputs}
+      </div>
+    </fieldset>`
+  )
+}
+
+const getTaskEditTmplt = (classList = {classes: ['black'], text: 'This is example of new task, you can set date and time.'}) => {
+  const classes = getClassList(classList.classes, 'card');
+
+                // ${getDateInputTmplt({date: new Date(2020, 10, 8, 16, 5)})}
+  return (
+    `<article class="card card--edit ${classes}">
+      <form class="card__form" method="get">
+        <div class="card__inner">
+          <div class="card__color-bar">
+            <svg width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <label>
+              <textarea
+                class="card__text"
+                placeholder="Start typing your text here..."
+                name="text"
+              >This is example of new task, you can set date and time.</textarea>
+            </label>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                ${getDateInputTmplt({date: new Date(2020, 10, 8, 16, 5)})}
+
+                ${getRepeatInputTmplt('1001001')}
+              </div>
+            </div>
+
+            <div class="card__colors-inner">
+              <h3 class="card__colors-title">Color</h3>
+              <div class="card__colors-wrap">
+                <input
+                  type="radio"
+                  id="color-black-1"
+                  class="card__color-input card__color-input--black visually-hidden"
+                  name="color"
+                  value="black"
+                  checked
+                />
+                <label
+                  for="color-black-1"
+                  class="card__color card__color--black"
+                  >black</label
+                >
+                <input
+                  type="radio"
+                  id="color-yellow-1"
+                  class="card__color-input card__color-input--yellow visually-hidden"
+                  name="color"
+                  value="yellow"
+                />
+                <label
+                  for="color-yellow-1"
+                  class="card__color card__color--yellow"
+                  >yellow</label
+                >
+                <input
+                  type="radio"
+                  id="color-blue-1"
+                  class="card__color-input card__color-input--blue visually-hidden"
+                  name="color"
+                  value="blue"
+                />
+                <label
+                  for="color-blue-1"
+                  class="card__color card__color--blue"
+                  >blue</label
+                >
+                <input
+                  type="radio"
+                  id="color-green-1"
+                  class="card__color-input card__color-input--green visually-hidden"
+                  name="color"
+                  value="green"
+                />
+                <label
+                  for="color-green-1"
+                  class="card__color card__color--green"
+                  >green</label
+                >
+                <input
+                  type="radio"
+                  id="color-pink-1"
+                  class="card__color-input card__color-input--pink visually-hidden"
+                  name="color"
+                  value="pink"
+                />
+                <label
+                  for="color-pink-1"
+                  class="card__color card__color--pink"
+                  >pink</label
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="card__status-btns">
+            <button class="card__save" type="submit">save</button>
+            <button class="card__delete" type="button">delete</button>
+          </div>
+        </div>
+      </form>
+    </article>`
+  );
+}
+
+const getTaskListTmplt = () => {
+  const cards = CARDS
+    .reduce((cards,classes) => {
+        return cards + `${getTaskCardTmplt(classes)}`;
+    },``)
+
+  return (
+   `${getTaskEditTmplt()}
+    ${cards}`
+  );
+}
+
+const getBoardTmplt = () => {
+  return (
+    `<section class="board container">
+      ${getBoardFilterTmplt()}
+      <div class="board__tasks">
+        ${getTaskListTmplt()}
+      </div>
+      ${getBtnMoreTmplt()}
+    </section>`
+  );
+}
+
+const getBtnMoreTmplt = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+}
+
+const render = (elem, template, position = 'beforeend') => {
+  elem.insertAdjacentHTML(position, template);
+}
+
+const main = document.querySelector('.main');
+const mainControl = document.querySelector('.main__control');
+
+render(mainControl,getControlTmplt());
+render(main,getFilterTmplt());
+render(main,getBoardTmplt());
